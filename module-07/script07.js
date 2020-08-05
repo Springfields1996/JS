aa32bf38c42d6cd6f99363d483b920dd455cd12c
Bezzubov Artem <66363533+Springfields1996@users.noreply.github.com>
2020-08-05 13:27:14 +0300

    Last fixes in homework

--- module-07/script07.js
+++ module-07/script07.js
@@ -26,35 +26,34 @@
 
 // HOMEWORK, task 03
 
-// const images = [
-//   {
-//     url:
-//       "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-//     alt: "White and Black Long Fur Cat",
-//   },
-//   {
-//     url:
-//       "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-//   },
-//   {
-//     url:
-//       "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-//     alt: "Group of Horses Running",
-//   },
-// ];
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
 
-// const gallery = document.querySelector("#gallery");
-// gallery.className = "photos";
-// const photos = images.reduce(
-//   (acc, photo) =>
-//     acc +
-//     `<li><img class = 'photo' src = '${photo.url}'
-//   alt = '${photo.alt}'></li>`,
-//   ""
-// );
-// console.log(photos);
-// gallery.insertAdjacentHTML("afterbegin", photos);
+const gallery = document.querySelector("#gallery");
+gallery.className = "photos";
+const photos = images.reduce(
+  (acc, photo) =>
+    acc +
+    `<li><img class = 'photo' src = '${photo.url}'
+  alt = '${photo.alt}'></li>`,
+  ""
+);
+gallery.insertAdjacentHTML("afterbegin", photos);
 
 // HOMEWORK, task 04
 
